fa9195f70769f9afb8bc7a4b688ce07900ca7b62
harwarl <harwarl87@gmail.com>
2024-06-17 14:36:58 +0100

    update: loggers info for all services

--- apps/fmpm-auth/src/main.ts
+++ apps/fmpm-auth/src/main.ts
@@ -3,17 +3,25 @@ import { AppModule } from './app/app.module';
 import { RabbitMQService } from '@fmpm/modules'; // Adjust the import path
 import { Queues } from '@fmpm/constants';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
 
   const configService = app.get(ConfigService);
   const rabbitMQService = app.get(RabbitMQService);
 
+  const port = configService.get<number>('AUTH_PORT');
   app.connectMicroservice(rabbitMQService.getRmqOptions(Queues.AUTH_QUEUE));
+
   await app.startAllMicroservices();
 
-  await app.listen(configService.get('AUTH_PORT'));
+  await app.listen(port);
+  Logger.log(`
+    🚀 Transaction Service is running on: http://localhost:${port}/${globalPrefix}
+    `);
 }
 
 bootstrap();
--- apps/fmpm-transaction-order-e2e/jest.config.ts
+++ apps/fmpm-transaction-order-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'fmpm-transaction-e2e',
+  displayName: 'fmpm-transaction-order-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -15,5 +15,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/fmpm-transaction-e2e',
+  coverageDirectory: '../../coverage/fmpm-transaction-order-e2e',
 };
